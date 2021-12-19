504b0c974ba8843ebe9d8c807b3d6a93e5c7dc9f
Yurii Rybitskyi <67706222+Hookae1@users.noreply.github.com>
2021-12-19 19:05:33 +0200

    Update setup_tomcat.sh

--- setup_tomcat.sh
+++ setup_tomcat.sh
@@ -11,7 +11,7 @@ wget https://tomcat.apache.org/tomcat-7.0-doc/appdev/sample/sample.war
 sudo systemctl stop tomcat9
 
 #Setup sample.war 
-sudo mv sample.war /var/liv/tomcat9/webapps/ && sudo rm -rf ROOT && sudo mv sample ROOT
+sudo mv sample.war /var/lib/tomcat9/webapps/ && sudo rm -rf ROOT && sudo mv sample ROOT
 
 #Start tomcat
 sudo systemctl start tomcat9 & sudo systemctl enable tomcat9
